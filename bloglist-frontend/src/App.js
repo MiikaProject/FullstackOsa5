556116d437166ce4981d40d262b6f0230b518501
Miika <miika.roiha@gmail.com>
2019-02-23 02:01:50 +0200

    logout

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -3,14 +3,19 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
-const Logged = ({blogs,user}) => {
+const Logged = ({blogs,user,setUser}) => {
   
+  const clicked = () =>{
+    setUser(null)
+    
+  }
   
   return (
     <div>
     
       <h2>blogs</h2>
       <p> {user.name}  logged in</p>
+      <button onClick={clicked}>logout</button>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
@@ -96,7 +101,7 @@ const App = () => {
 
   return (
     <div>
-    <Logged blogs={blogs} user={user}/>
+    <Logged blogs={blogs} user={user} setUser={setUser}/>
     </div>
   )
 }
